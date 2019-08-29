9566e7659f2af451432f2e7cb996bfebda4ec9df
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-08-29 16:46:07 -0400

    .

--- Prototypes/js/index.js
+++ Prototypes/js/index.js
@@ -1,32 +1,15 @@
-
 rubrique('international');
 $('#frist').css('border-bottom', '2px solid #db5e11');
 
 function affMenu(){
    $(".all" ).hide();
    $(".menu").show();
-   $("#nav1").html('<h1>Menu</h1><img src="img/image 5.png" alt="parametres" class="icones" id="cog" onclick="affparam()">');
-   $('.fbi1').attr('src', 'img/icons8-accueil-50.png');
-   $('.fbi2').attr('src', 'img/icons8-menu-48.png');
-   $('.fp2' ).css('color', 'black');
-   $('.fp1' ).css('color', 'black');  
 }
 
 function affAccueil(){
    $('li'    ).css("border-bottom", "0px solid black");
    $(".all"  ).hide();
    $("output").show();
-   $("#nav1" ).html('<h1>NewsAntilles</h1>');
-   $('.fbi1' ).attr('src', 'img/icons8-accueil-50.png');
-   $('.fbi2' ).attr('src', 'img/icons8-menu-48.png');
-   $('.fp1'  ).css('color', 'black');
-   $('.fp2'  ).css('color', 'black');
-}
-     
-function affparam(){
-   $(".all"  ).hide();
-   $("#param").show();
-   $("#nav1" ).html('<img onclick="affMenu()" class="icones right" src="img/back.png" alt="retour"><h1>Paramètres</h1>');
 }
 
 function rubrique(a){
@@ -34,8 +17,6 @@ function rubrique(a){
     try {
        if (event.currentTarget.classList.contains('under'))
        {event.currentTarget.style.borderBottom = '2px solid #db5e11'; }
-        bob = event.currentTarget.innerText;
-        $('#nav1 h1').html(bob);
    } catch { }
 
    b=0;
@@ -53,7 +34,6 @@ function rubrique(a){
             di.className = "article section" + b;
 			
             di = '<div class="article section' +b+ '" onclick="lire(`' +tit+ '`)"></div>';
-            //di = `<div class="article section${b}" onclick="lire('${tit}')"></div>`;
             $('output').append(di);
 
             h2 = document.createElement('h2');
@@ -73,30 +53,16 @@ function rubrique(a){
    });
 }
 
-
 function lire(a){
-	f = "<iframe class='iframe' src='" +a+ "'>";
-	$('.screen').append(f);
-   document.getElementById('nav1').insertAdjacentHTML("afterbegin", '<img onclick="closeFrame()" class="icones right retour" src="img/back.png" alt="retour">');
-   $('#nav2').html('<img onclick="closeFrame()" class="icones right retour" src="img/back.png" alt="retour"><h1>NewsAntilles</h1>');
+   f = "<div class='page-article'></div>";
+   g = '<div class="page-bar" onclick="closeFrame()"><img src="img/back.png" alt="retour" class="button-menu"></div>';
+   h = "<iframe class='iframe' src='" +a+ "'></iframe>";
+   $('body').append(f);
+   $('.page-article').append(g);
+   $('.page-article').append(h);
 }  
 
 function closeFrame(){
-   g = document.getElementsByClassName('iframe');
-   fa = g.length;
-   
-   for(i= g.length; i>0; i--){
-      g[i-1].parentNode.removeChild(g[i-1]);
-   }
-
-
-   h = document.getElementsByClassName('retour');
-   fi = h.length;
-   
-   for(j= h.length; j>0; j--){
-      h[j-1].parentNode.removeChild(h[j-1]);
-   }
-   
-   $('#nav2').html('<img onclick="affMenu()" class="right icones" src="img/icons8-menu-24.png" alt=""><h1 class="titre" id="titre2">NewsAntilles</h1>')
-   
+   g = document.getElementsByClassName('page-article');
+   g[0].parentNode.removeChild(g[0]);
 }
