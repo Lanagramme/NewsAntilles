b42e8b95013bdba20664a2784255f7142954d7c6
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-09-04 08:57:10 -0400

    .

--- NewsAntilles/platforms/android/app/src/main/assets/www/js/index.js
+++ NewsAntilles/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,24 +1,85 @@
+rubrique('societe');
+$('#frist').css('border-bottom', '2px solid #db5e11');
+$(".splash").delay( 1000 ).fadeOut( 400 );
+$('.parametres').click(function() {
+   $('#param').toggle();
+});
+$('.propos').click(function() {
+   $('#contact').toggle();
+});
+$('.vers').click(function() {
+   $('#build').toggle();
+});
+function affMenu(){
+   $(".all" ).hide();
+   $(".menu").show();
+}
 
-    fetch("https://www.bfmtv.com/rss/politique/").then((res) => {
-      res.text().then((xmlTxt) => {
-        var domParser = new DOMParser();
-        let doc = domParser.parseFromString(xmlTxt, 'text/xml');
-        doc.querySelectorAll('item').forEach((item) => {
-           
-            let h2 = document.createElement('h2');
+function affAccueil(){
+   $('li'    ).css("border-bottom", "0px solid black");
+   $(".all"  ).hide();
+   $("output").show();
+}
+
+function rubrique(a){
+   affAccueil();
+   under = document.getElementsByClassName('under');
+   for (const item of under) {
+      if (item.classList.contains(a))
+      {item.style.borderBottom = '2px solid #db5e11';}
+   }
+
+
+   b=0;
+   $('output').html('');
+   spin = '<div class="lds-ellipsis" style="margin-left: 45vw; margin-top: 40vh;"><div></div><div></div><div></div><div></div></div>';
+   $('output').append(spin);
+   fetch("https://www.bfmtv.com/rss/" + a + "/").then((res) => {
+   res.text().then((xmlTxt) => {
+      var domParser = new DOMParser();
+      doc = domParser.parseFromString(xmlTxt, 'text/xml');
+      try {
+         $('.lds-ellipsis').hide();
+      } catch (error) {
+         
+      }
+         doc.querySelectorAll('item').forEach((item) => {
+      
+            tit = item.querySelector('link').textContent;
+			
+            di = document.createElement('div');
+            di.className = "article section" + b;
+			
+            di = '<div class="article section' +b+ '" onclick="lire(`' +tit+ '`)"></div>';
+            $('output').append(di);
+
+            h2 = document.createElement('h2');
             h2.textContent = item.querySelector('title').textContent;
-            document.querySelector('output').appendChild(h2);
-            
-            let p = document.createElement('p');
-            p.textContent = item.querySelector('description').textContent;
-            document.querySelector('output').appendChild(p);
-            
-            let a = document.createElement('a');
-            let linkText = document.createTextNode("Lire plus >>");
-            a.appendChild(linkText);
-            a.title = "my title text";
-            a.href = item.querySelector('link').textContent;
-            document.querySelector('output').appendChild(a);
-           })
-         })
-    })
+            h2.className = "titreA";
+      
+            $('.section' + b).append(h2);
+
+            p = document.createElement('p');
+            p.innerHTML = item.querySelector('description').textContent;
+            p.className = "texteA";
+      
+            $('.section' + b).append(p);
+            b++;
+         });
+      });
+   });
+}
+
+function lire(a){
+   f = "<div class='page-article'></div>";
+   g = '<div class="page-bar" onclick="closeFrame()"><img src="img/back.png" alt="retour" class="button-menu"></div>';
+   h = "<iframe class='iframe' src='" +a+ "'></iframe>";
+   $('body').append(f);
+   $('.page-article').append(g);
+   $('.page-article').append(h);
+}  
+
+function closeFrame(){
+   g = document.getElementsByClassName('page-article');
+   g[0].parentNode.removeChild(g[0]);
+}
